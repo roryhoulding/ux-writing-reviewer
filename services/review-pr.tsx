bb812baebc1ef652b1ea20c13b4bd5a462b4ce1d
Rory Houlding <rohoulding@googlemail.com>
2025-08-31 18:16:20 -0400

    Update review-pr to fetch pull request data

--- services/review-pr.tsx
+++ services/review-pr.tsx
@@ -1,9 +1,43 @@
 const fs = require('fs');
+const { Octokit } = require('octokit');
 const path = process.env.GITHUB_EVENT_PATH;
 
-if (path) {
+async function main() {
+  if (!path) {
+    console.error('GITHUB_EVENT_PATH is not set.');
+    return;
+  }
+  
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  
   const eventData = JSON.parse(fs.readFileSync(path, 'utf8'));
+  const { pull_request, repository } = eventData;
+  const { number } = pull_request;
+
+  const owner = repository?.owner?.login;
+  const repo = repository?.name;
+  
+  if (!owner) {
+    console.error('Could not determine owner');
+    return;
+  }
+
+  if (!repo) {
+    console.error('Could not determine repo name');
+    return;
+  }
+  
+  const reviews = await octokit.rest.pulls.listReviews({
+    owner: owner,
+    repo: repo,
+    pull_number: number,
+  });
+
+  console.log('Reviews:', reviews.data);
+  
   console.log('Event Data:', eventData);
-} else {
-  console.error('GITHUB_EVENT_PATH is not set.');
-}
+} 
+
+main().catch(console.error);
